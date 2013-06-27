452ad78c9e9e9c5ac4ecc19aac695ad057e9bbd5
cloudnull <kevin.carter@rackspace.com>
2013-06-27 10:13:06 -0500

    updated for new naming scheme

--- PKG-INFO
+++ PKG-INFO
@@ -1,10 +1,10 @@
 Metadata-Version: 1.0
-Name: prettytable
+Name: lprettytable
 Version: 0.5
 Summary: A simple Python library for easily displaying tabular data in a visually appealing ASCII table format
-Home-page: http://www.luke.maurits.id.au/software/prettytable
+Home-page: https://github.com/cloudnull/lprettytable
 Author: Luke Maurits
 Author-email: luke@maurits.id.au
+Maintainer: Kevin Carter
+Maintainer-email: kevin.carter@rackspace.com
 License: http://www.luke.maurits.id.au/software/bsdlicense.txt
-Description: UNKNOWN
-Platform: UNKNOWN
--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 from setuptools import setup
 
 setup(
-    name='prettytable',
+    name='lprettytable',
     version='0.5',
     description='A simple Python library for easily displaying tabular data in a visually appealing ASCII table format',
     author='Luke Maurits',
     author_email='luke@maurits.id.au',
-    url='http://www.luke.maurits.id.au/software/prettytable',
+    url='https://github.com/cloudnull/lprettytable',
     license='http://www.luke.maurits.id.au/software/bsdlicense.txt',
-    py_modules=['prettytable']
+    py_modules=['lprettytable']
 )
